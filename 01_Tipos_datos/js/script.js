44c060e70e428a2bf5f5ab1bd088923c6a95dab8
soniaMota <s.mota.diaz@iescristobaldemonroy.es>
2020-06-10 18:27:24 +0200

    comienzo del curso

---
+++ 01_Tipos_datos/js/script.js
@@ -0,0 +1,17 @@
+// Declaración e inicialización de variables
+let numero = 5;
+
+let palabra = 'Hola Mundo';
+
+let respuesta = true;
+
+// Declaración e inicialización de constante
+const PI = 3.14;
+
+// modificamos el valor de la variable 
+numero = 54; 
+palabra = "Adiós"; // es indiferente que usemos comillas simples o comillas dobles,
+//aunque hayamos usado una de ellas en la inicialización de la misma variable
+
+// Mostrar por consola el valor de la variable indicada
+console.log(palabra);
---
+++ 02_Tipos_operadores/js/script.js
@@ -0,0 +1,48 @@
+/*Operadores matemáticos*/
+// Declaración e inicialización de variables
+/*let a = 5;
+let b = 2;
+
+let usuario = 'Sonia';
+
+console.log(a+b); //Devuelve 7
+console.log(a-b); //Devuelve 3
+console.log(a*b); //Devuelve 10
+console.log(a/b); //Devuelve 2.5
+console.log(a%b); //Devuelve 1 (que sería el resto de la división)
+
+console.log("Hola " + usuario); */
+
+/*Operadores Incremento/Decremento*/
+let a = 5;
+//Incremento
+console.log(a); //Devuelve 5
+console.log(a++); //Devuelve la variable (5) y luego guarda el valor incrementado (6) en dicha variable
+console.log(a); //Devuelve 6, ya que es el valor ahora guardado
+console.log(++a); //Devuelve 7 porque ha incrementado en 1 el valor, que era 6, y luego lo devuelve
+console.log(a); //Devuelve 7, que es el valor que en este momento tiene a
+//Decremento
+console.log(a--); //Devuelve 7, que es el valor de la variable. Luego decrementa y guarda el valor
+console.log(a); //Devuelve 6, que es el valor guardado después del decremento
+console.log(--a); //Devuelve 5, porque primero decrementa en 1 el valor (6) y luego lo devuelve
+console.log(a); //Devuelve 5, que es el valor guardado en la variable
+
+
+/*Operadores Asignación*/
+// let a = 10;
+//Suma y asignación
+/*// a = a + 3;
+a += 3;
+console.log(a); //En ambos casos devuelve 13*/
+//Resta y asignación
+/*a -=3; //Sería lo mismo que a = a - 3;
+console.log(a); //Devuelve 7*/
+//Multiplicación y asignación
+/*a *= 3; //Sería lo mismo que a = a * 3;
+console.log(a); //Devuelve 30*/
+//División y asignación
+/*a /= 3; //Sería igual que a = a / 3;
+console.log(a); //Devuelve 3.33*/
+//Módulo y asignación
+/*a %= 3; //Sería igual que a = a % 3;
+console.log(a); //Devuelve 1 que es el resto que resulta de la división entera de 10 entre 3*/

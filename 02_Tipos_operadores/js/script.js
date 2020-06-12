eb9ae4b1e3d22a3acfc1a69ccf8f87f9dd50ae55
soniaMota <s.mota.diaz@iescristobaldemonroy.es>
2020-06-12 10:11:46 +0200

    retirados comentarios del código

--- 02_Tipos_operadores/js/script.js
+++ 02_Tipos_operadores/js/script.js
@@ -11,21 +11,21 @@ console.log(a*b); //Devuelve 10
 console.log(a/b); //Devuelve 2.5
 console.log(a%b); //Devuelve 1 (que sería el resto de la división)*/
 
-console.log("Hola " + usuario); 
+console.log("Hola " + usuario);
 
 /*Operadores Incremento/Decremento*/
-//let a = 5;
-//Incremento
-/*console.log(a); //Devuelve 5
-console.log(a++); //Devuelve la variable (5) y luego guarda el valor incrementado (6) en dicha variable
-console.log(a); //Devuelve 6, ya que es el valor ahora guardado
-console.log(++a); //Devuelve 7 porque ha incrementado en 1 el valor, que era 6, y luego lo devuelve
-console.log(a); //Devuelve 7, que es el valor que en este momento tiene a
-//Decremento
-console.log(a--); //Devuelve 7, que es el valor de la variable. Luego decrementa y guarda el valor
-console.log(a); //Devuelve 6, que es el valor guardado después del decremento
-console.log(--a); //Devuelve 5, porque primero decrementa en 1 el valor (6) y luego lo devuelve*/
-//console.log(a); //Devuelve 5, que es el valor guardado en la variable
+// let a = 5;
+// Incremento
+// console.log(a); //Devuelve 5
+// console.log(a++); //Devuelve la variable (5) y luego guarda el valor incrementado (6) en dicha variable
+// console.log(a); //Devuelve 6, ya que es el valor ahora guardado
+// console.log(++a); //Devuelve 7 porque ha incrementado en 1 el valor, que era 6, y luego lo devuelve
+// console.log(a); //Devuelve 7, que es el valor que en este momento tiene a
+// //Decremento
+// console.log(a--); //Devuelve 7, que es el valor de la variable. Luego decrementa y guarda el valor
+// console.log(a); //Devuelve 6, que es el valor guardado después del decremento
+// console.log(--a); //Devuelve 5, porque primero decrementa en 1 el valor (6) y luego lo devuelve
+// console.log(a); //Devuelve 5, que es el valor guardado en la variable
 
 
 /*Operadores Asignación*/
